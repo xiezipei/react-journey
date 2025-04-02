c6dcd69e69c3ae11e38f48d41a5a8d08403c8c5e
xiezipei <zipeixie@gmail.com>
2025-04-02 14:07:50 +0800

    feat: 添加 @ant-design/v5-patch-for-react-19 依赖，并在 App 组件中集成消息提示功能

--- 05/my-app-antd-tailwind/src/App.jsx
+++ 05/my-app-antd-tailwind/src/App.jsx
@@ -1,11 +1,21 @@
 import './App.css'
-import { Button } from 'antd'
+import { Button, message } from 'antd'
 
 function App() { 
+
+  const [messageApi, contextHolder] = message.useMessage();
+
+  const showInfo = () => {
+    messageApi.info('Hello, Ant Design!');
+  };
+
   return (
     <>
       <div className="App">
-        <Button type='primary'>Antd 按钮</Button>
+        {/* 验证 antd */}
+        {contextHolder}
+        <Button type='primary' onClick={showInfo}>Antd 按钮</Button>
+        {/* 验证 tailwind */}
         <h1 class="text-3xl font-bold underline">
           Hello Tailwind!
         </h1>
